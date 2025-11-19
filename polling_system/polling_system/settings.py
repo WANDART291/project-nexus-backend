e94fe515d4ea8972de65f57a1dfce5cdfbd7e65a
Wandile Khanyile <wandilekhanyile63@gmail.com>
2025-11-19 15:59:58 -0800

    Update: Setup PostgreSQL database and fixed migrations

--- polling_system/polling_system/settings.py
+++ polling_system/polling_system/settings.py
@@ -123,11 +123,15 @@ WSGI_APPLICATION = 'polling_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "project_nexusdb",
+        "USER": "postgres",
+        "PASSWORD": "admin123",  
+        "HOST": "localhost",
+        "PORT": "5432",
     }
-} 
+}
 
 
 # Password validation
